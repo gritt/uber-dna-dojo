8a7dc87534e490842aa66765a07301e811e13dc3
Gilvan <gilvanritter@gmail.com>
2018-03-18 23:51:46 -0300

    under dev, find remaining permutations of gene from a given index

--- main.go
+++ main.go
@@ -22,48 +22,77 @@ type results []string
 
 func main() {
 
-	// DNA slice
-	ds := dnaFromFile()
+	// the dna slice
+	ds := getDNAFromFile()
 
-	// Genes map with their permutations
+	if !ds.isValid() {
+		fmt.Println("error: DNA code has less than 6 nucleobases:")
+		os.Exit(1)
+	}
+
+	// genes map with their permutations
 	gm := map[string]genePermutations{
 		"ACT": genePermutations{"CGT", "AGT"},
 		"AGT": genePermutations{"CGT", "AGT"},
 		"CGT": genePermutations{"ACT", "AGT"},
 	}
 
-	ds.findShortestOccurence(gm)
+	ds.findShortestOccurrence(gm)
 }
 
-func (ds dna) findShortestOccurence(gm map[string]genePermutations) {
+func getDNAFromFile() dna {
+	bs, err := ioutil.ReadFile(os.Args[1])
 
-	for fg, gps := range gm {
-
-		//todo
-		//channels
-		//findGeneOccurenceIndexes
-		//when receives a result, start new routine for the subsequences search
-		//with the index got from the channel, and with this first gene(fg)
-
-		fmt.Println("permutations of gene", fg, "are", gps)
-
-		ch := make(chan int)
-
-		go ds.findGeneOccurenceIndexes(fg, ch)
-
-		i := <-ch
-
-		fmt.Println("found gene", fg, "at index", i)
+	if err != nil {
+		fmt.Println("coult not open file:", err)
+		os.Exit(1)
 	}
 
-	// fmt.Println(gm)
-	// fmt.Println(ds)
+	s := strings.Split(string(bs), "")
+
+	return dna(s)
 }
 
-func (ds dna) findGeneOccurenceIndexes(g string, ch chan int) {
+func (ds dna) isValid() bool {
+	if len(ds) < 6 {
+		return  false
+	}
+	return  true
+}
 
-	// DNA length
+func (ds dna) findShortestOccurrence(gpm map[string]genePermutations) {
+
+	// g := ACT or AGT or CGT
+	for g, gps := range gpm {
+
+		//findGeneOccurrenceIndexes
+		//when receives a result, start new routine for the subsequences search
+		//with the index got from the channel, and with this first gene(g)
+
+		fmt.Println("permutations of gene", g, "are", gps)
+
+		// TODO: a channel to transport integers
+		//ch := make(chan int)
+
+		// TODO: run search in a new thread
+		//go ds.findGeneOccurrenceIndexesConcurrent(g, ch)
+
+		// TODO: listen for indexes, start a new search by the current gene from there
+		//i := <-ch
+		//fmt.Println("found gene", g, "at index", i)
+
+		//idxs := ds.findGeneOccurrenceIndexes(g)
+		//fmt.Println("indexes with permutation", g, ":", idxs)
+
+	}
+}
+
+func (ds dna) findGeneOccurrenceIndexes(g string) []int {
+
+	// dna length
 	dsl := len(ds)
+
+	var idxs []int
 
 	for i := 0; i < dsl; i++ {
 
@@ -75,31 +104,40 @@ func (ds dna) findGeneOccurenceIndexes(g string, ch chan int) {
 		cgs := string(ds[i] + ds[i+1] + ds[i+2])
 		if cgs == g {
 
-			ch <- i
-
-			// todo
-			// found?
-			// communicate through channel the index which was found
-			// start subsequences search from there witht the permutations
+			// slice of all indexes with matches
+			idxs = append(idxs, i)
 
 			i += 2
 		}
-		// fmt.Println(i)
-		// fmt.Println(gs)
+	}
+
+	return idxs
+}
+
+func (ds dna) findGeneOccurrenceIndexesConcurrent(g string, ch chan int) {
+
+	// dna length
+	dsl := len(ds)
+
+	for i := 0; i < dsl; i++ {
+
+		if (i + 2) >= dsl {
+			i++
+			continue
+		}
+
+		cgs := string(ds[i] + ds[i+1] + ds[i+2])
+
+		if cgs == g {
+
+			// notify channel a match was found at this index
+			ch <- i
+
+			i += 2
+		}
 	}
 }
 
-func dnaFromFile() dna {
-	bs, err := ioutil.ReadFile(os.Args[1])
-	if err != nil {
-		fmt.Println("Error:", err)
-		os.Exit(1)
-	}
-	s := strings.Split(string(bs), "")
-
-	if len(s) < 6 {
-		os.Exit(1)
-	}
-
-	return dna(s)
+func (ds dna) findRemainingGenesFromIndex(idx int, gpm map[string]genePermutations) string {
+	return "hello from the other side"
 }
